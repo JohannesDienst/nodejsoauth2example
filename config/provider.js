b9c68f65693f55e0488821c433dc0efef10ec41f
root <root@localhost.localdomain>
2015-04-09 21:02:19 +0200

    New files and changes for social OAuth

---
+++ config/provider.js
@@ -0,0 +1,15 @@
+var ids = {
+  twitter: {
+    consumerKey: 'PNyrzXI6PO3fhdF2AEQqx7iFP',
+    consumerSecret: 'n27WCX0s2vgO3xrTkFyzffHsAs1RlpbUw3BRhsg0bJpNarbwLb',
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  },
+
+  googleAuth : {
+    clientID : '207759864372-9nk0tltm4mbr9ttfi7gvcse0ilkhm11k.apps.googleusercontent.com',
+    clientSecret : 'bm5q19-hFUzzYuclIkvsRP75',
+    callbackURL : 'http://127.0.0.1:3000/auth/google/callback'
+  }
+}
+
+module.exports = ids
